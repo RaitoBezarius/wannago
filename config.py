a455eeec9287185f12aa1ffc460683cbe8336703
Shir0kamii <alexandrebonnetain@gmail.com>
2015-05-15 01:40:22 +0200

    3 commits rebased concerning config.py
    add config.py
    add exit hook for registering config when exiting
    Add methods to set and get Config attributes safely

---
+++ config.py
@@ -0,0 +1,33 @@
+import configparser, atexit
+
+CONFIG_FILE = "wannago.conf"
+
+class Config(configparser.ConfigParser):
+    def __init__(self, config_file=CONFIG_FILE):
+        super().__init__()
+        self.read(config_file)
+        self.config_file = config_file
+        atexit.register(Config.writeConfig, self)
+
+    def writeConfig(self):
+        with open(self.config_file, 'w') as file:
+            self.write(file)
+
+    def setConfig(self, section, name, value, overwrite=True):
+        if not section in self:
+            self[section] = {}
+        if not name in self[section] or overwrite:
+            self[section][name] = value
+
+    def getConfig(self, section, name):
+        if not section in self or not name in self[section]:
+            return None
+        return self[section][name]
+
+    def setToken(self, token):
+        self.setConfig('Navitia', 'Token', token)
+
+    def getToken(self):
+        return self.getConfig('Navitia', 'Token')
+
+defaultConfig = Config()

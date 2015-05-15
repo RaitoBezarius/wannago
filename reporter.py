caa976bfd0c43896773aa55b2c8c6fa5e3c8b045
Shir0kamii <alexandrebonnetain@gmail.com>
2015-05-15 04:47:38 +0200

    Change the presentation of the result, with one step by line

--- reporter.py
+++ reporter.py
@@ -4,11 +4,11 @@ def report_journey(api, from_place, to_place, without=None):
     resp = api.compute_journey(from_place, to_place)
     journey = Journey(resp['journeys'][0])
 
-    message = '[{duration} mn] {path}'
+    message = '[{duration} mn]\n{path}'
     subpaths = []
     for section in journey.sections:
         if section.type == 'public_transport':
-            subpath = '{net_label} - {to_place} [{duration} mn]'.\
+            subpath = '==> {net_label} - {to_place} [{duration} mn]'.\
             format(net_label=section.network_label, from_place=section.from_section.name,
                    to_place=section.to_section.name,
                    duration=section.duration)
@@ -16,6 +16,6 @@ def report_journey(api, from_place, to_place, without=None):
         else:
             print ('[DEBUG]: what to do with this section: {section}'.format(section=section))
 
-    message = message.format(duration=journey.duration, path=' ==> '.join(subpaths))
+    message = message.format(duration=journey.duration, path='\n'.join(subpaths))
 
     print (message)

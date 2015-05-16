2caf807912a7ae7e3449c2ac3a89b3913edfb715
Raito Bezarius <masterancpp@gmail.com>
2015-05-16 23:35:28 +0200

    Add basic support for departures / arrival datetimes (which seems falsy...) Using Arrow Python lib.

--- src/reporter.py
+++ src/reporter.py
@@ -17,7 +17,7 @@ def report_journey(api, from_place, to_place, without=None):
     resp = api.compute_journey(from_place, to_place)
     journey = Journey(resp['journeys'][0])
 
-    message = '[{duration} mn]\n{path}'
+    message = '[{duration} mn] - [{start}]\n{path}\n[{end}]'
     subpaths = []
     for section in journey.sections:
         if section.type == 'public_transport':
@@ -32,7 +32,10 @@ def report_journey(api, from_place, to_place, without=None):
         else:
             print ('[DEBUG]: what to do with this section: {section}'.format(section=section))
 
-    message = message.format(duration=journey.duration, path='\n'.join(subpaths))
+    message = message.format(duration=journey.duration,
+            start=journey.start_time.format('HH:mm:ss'),
+            end=journey.end_time.format('HH:mm:ss'),
+            path='\n'.join(subpaths))
 
     print (message)
     return message

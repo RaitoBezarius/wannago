6e07bc618413872647e6ce9eac03cdcf3ba056c5
Raito Bezarius <masterancpp@gmail.com>
2015-05-17 18:56:15 +0200

    Format time: {hour}h{minutes} As asked by @Shir0kamii

--- src/reporter.py
+++ src/reporter.py
@@ -17,7 +17,7 @@ def report_journey(api, from_place, to_place, without=None):
     resp = api.compute_journey(from_place, to_place)
     journey = Journey(resp['journeys'][0])
 
-    message = '[{duration} mn] - [{start}]\n{path}\n[{end}]'
+    message = '[{duration} mn] - [{start_hour}h{start_minutes}]\n{path}\n[{end_hour}h{end_minutes}]'
     subpaths = []
     for section in journey.sections:
         if section.type == 'public_transport':
@@ -33,8 +33,10 @@ def report_journey(api, from_place, to_place, without=None):
             print ('[DEBUG]: what to do with this section: {section}'.format(section=section))
 
     message = message.format(duration=journey.duration,
-            start=journey.start_time.format('HH:mm:ss'),
-            end=journey.end_time.format('HH:mm:ss'),
+            start_hour=journey.start_time.format('HH'),
+            start_minutes=journey.start_time.format('mm'),
+            end_hour=journey.end_time.format('HH'),
+            end_minutes=journey.end_time.format('mm'),
             path='\n'.join(subpaths))
 
     print (message)

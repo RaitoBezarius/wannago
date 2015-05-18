df8d7795c5b6b01af4f426cdb6f7ab8f19e11b06
Raito Bezarius <masterancpp@gmail.com>
2015-05-19 00:01:56 +0200

    Add some tests to improve coverage

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,6 @@
-import sys
+from src.config import Config
 import unittest
 from unittest import mock
-import json
 
 class TestLocationManager(unittest.TestCase):
     
@@ -14,4 +13,27 @@ class TestLocationManager(unittest.TestCase):
         self.assertEqual(manager.default_zone, 'fr-idf')
         defaultConfigMock.getConfig.assert_called_with('Coverage', 'DefaultZone',
                 defaultValue='fr-idf', writeIfMissing=True)
-            
+    
+
+class TestConfigStore(unittest.TestCase):
+    
+    def test_set_config(self):
+        pass
+    
+    def test_get_config(self):
+        pass
+
+    def test_get_section(self):
+        pass
+
+    def test_set_token(self):
+        pass
+
+    def test_get_token(self):
+        pass
+
+    def test_write_config(self):
+        pass
+
+    def test_initialize_config(self):
+        pass
